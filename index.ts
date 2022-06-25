58cda1e1adb18a2cc3abf8c4092951268466a56b
Kamil <kamgre7@gmail.com>
2022-06-25 17:54:26 +0200

    Added multer, testing receiving form with photo

--- index.ts
+++ index.ts
@@ -1,10 +1,28 @@
 import cors from 'cors';
 import express, { json } from 'express';
 import rateLimit from 'express-rate-limit';
+import multer, { diskStorage } from 'multer';
 import { config } from './config/config';
 import { homeRouter } from './routes/home';
 
 const app = express();
+
+const fileProperty = {
+  filename: '',
+};
+
+const storage = diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    fileProperty.filename = file.originalname;
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 app.use(cors({
   origin: config.corsOrigin,
@@ -14,6 +32,16 @@ app.use(rateLimit({
   windowMs: 5 * 60 * 1000, // 5minutes
   max: 100, // Limit each IP to 100 requests per 'window' (here, per 5 minutes)
 }));
+
+app.post('/', upload.single('photo'), (req, res) => {
+  const {
+    name, description, quantity, price, sku, category,
+  } = req.body;
+  console.log({
+    name, description, quantity, price, sku, category,
+  }, fileProperty.filename);
+  res.send('img uploaded');
+});
 
 app.use('/', homeRouter);
 
--- routes/home.ts
+++ routes/home.ts
@@ -3,6 +3,11 @@ import { Router } from 'express';
 export const homeRouter = Router();
 
 homeRouter
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
   .get('/', (req, res) => {
+
+  })
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .post('/', (req, res) => {
 
   });
