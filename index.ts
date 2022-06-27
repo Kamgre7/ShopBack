e6a9f2475306a1089f11de4a3fecd7a1ad0326ab
Kamil <kamgre7@gmail.com>
2022-06-27 20:00:54 +0200

    Added /product route with mutler

--- index.ts
+++ index.ts
@@ -1,28 +1,11 @@
 import cors from 'cors';
 import express, { json } from 'express';
 import rateLimit from 'express-rate-limit';
-import multer, { diskStorage } from 'multer';
 import { config } from './config/config';
 import { homeRouter } from './routes/home';
+import { productsRouter } from './routes/produtct';
 
 const app = express();
-
-const fileProperty = {
-  filename: '',
-};
-
-const storage = diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    console.log(file);
-    fileProperty.filename = file.originalname;
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
 
 app.use(cors({
   origin: config.corsOrigin,
@@ -33,17 +16,8 @@ app.use(rateLimit({
   max: 100, // Limit each IP to 100 requests per 'window' (here, per 5 minutes)
 }));
 
-app.post('/', upload.single('photo'), (req, res) => {
-  const {
-    name, description, quantity, price, sku, category,
-  } = req.body;
-  console.log({
-    name, description, quantity, price, sku, category,
-  }, fileProperty.filename);
-  res.send('img uploaded');
-});
-
 app.use('/', homeRouter);
+app.use('/product', productsRouter);
 
 app.listen(3001, '127.0.0.1', () => {
   console.log('Listening on port http://localhost:3001');
