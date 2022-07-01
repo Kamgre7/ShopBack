1b21887bcad01929ec5bd65e840529166c8f77fd
Kamil <kamgre7@gmail.com>
2022-07-01 20:05:11 +0200

    Added validation functions and /category/:id routes

--- records/category.record.ts
+++ records/category.record.ts
@@ -1,6 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import { CategoryEntity, CategoryRecordResult } from '../types';
 import { pool } from '../utils/db';
+import { ValidationError } from '../utils/errors';
+import {
+  checkDescriptionLength, checkNameLength, createDate,
+} from '../utils/validation.entity';
 
 export class CategoryRecord {
   id?:string;
@@ -14,6 +18,10 @@ export class CategoryRecord {
   createdAt?: string;
 
   constructor(newCategoryEntity: CategoryEntity) {
+    checkNameLength(newCategoryEntity.name);
+    checkNameLength(newCategoryEntity.img);
+    checkDescriptionLength(newCategoryEntity.description);
+
     this.id = newCategoryEntity.id;
     this.name = newCategoryEntity.name;
     this.description = newCategoryEntity.description;
@@ -27,8 +35,13 @@ export class CategoryRecord {
   }
 
   async insert(): Promise<string> {
-    this.id = this.id ?? uuid();
-    this.createdAt = this.createdAt ?? new Date().toISOString().slice(0, 19).replace('T', ' ');
+    if (!this.id) {
+      this.id = uuid();
+    } else {
+      throw new ValidationError('Cannot insert something that is already inserted');
+    }
+
+    this.createdAt = this.createdAt ?? createDate();
 
     await pool.execute('INSERT INTO `product_category` VALUES(:id, :name, :description, :img, :createdAt)', {
       id: this.id,
@@ -41,7 +54,10 @@ export class CategoryRecord {
     return this.id;
   }
 
-  static async getOne(): Promise<void> {
-
+  static async getOne(id:string): Promise<CategoryEntity | null> {
+    const [findCategory] = await pool.execute('SELECT * FROM `product_category` WHERE `id`=:id', {
+      id,
+    }) as CategoryRecordResult;
+    return findCategory.length === 0 ? null : new CategoryRecord(findCategory[0]);
   }
 }
--- routes/category.ts
+++ routes/category.ts
@@ -3,19 +3,37 @@ import { CategoryEntityForm } from '../types';
 import { upload } from '../utils/storage';
 import { CategoryRecord } from '../records/category.record';
 import { filePathToFileName } from '../utils/filePathToFileName';
+import { ValidationError } from '../utils/errors';
 
 export const categoryRouter = Router();
 
 categoryRouter
-  .get('/', async (req, res) => {
+  .get('/', async (req, res):Promise<void> => {
     const allCategories = await CategoryRecord.getAll();
-    console.log(allCategories);
+    res.json(allCategories);
+  })
+
+  .get('/:id', async (req, res):Promise<void> => {
+    const category = await CategoryRecord.getOne(req.params.id);
+
+    if (category === null) {
+      throw new ValidationError('Cannot find category');
+    }
+
+    res.json(category);
   })
 
   .post('/form', upload.single('img'), async (req, res) => {
     const {
       name, description, imgFileName,
     }: CategoryEntityForm = req.body;
+
+    const allCategories = await CategoryRecord.getAll();
+    const findDuplicate = allCategories.filter((category) => category.name.toLowerCase() === name.toLowerCase());
+
+    if (findDuplicate.length > 0) {
+      throw new ValidationError('This name is already taken. Try other one.');
+    }
 
     const newCategory = new CategoryRecord({
       name,
